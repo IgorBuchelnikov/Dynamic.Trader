e88771ab85d28be18303d937ad907125d2151248
Igor Buchelnikov <igor_buchelnikov_github@mail.ru>
2021-03-31 16:56:05 +0300

    2.1.0

---
+++ ObservableComputationsEdition/ComputationsInBackgroundThread/Trader.Domain/Infrastucture/ExtentionMethods.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Trader.Domain.Infrastucture
+{
+	public static class ExtentionMethods
+	{
+		public static IEnumerable<IEnumerable<T>> Split<T>(this IEnumerable<T> items, int size)
+		{
+			int count = items.Count();
+			for (int i = 0; i < count; i += size) 
+			{ 
+				yield return items.Skip(i).Take(size); 
+			}
+		}
+	}
+}
